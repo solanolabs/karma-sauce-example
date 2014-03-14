026523f2122bdec4bec1c73b8e42f9e924566d2c
Chris Wren <chris@saucelabs.com>
2014-03-13 17:48:18 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-karma-sauce-example
-===================
+# karma-sauce-example
 
-An example of using the Karma test runner with Sauce Labs
+> An example of using the Karma test runner with Sauce Labs.
+
+To get started, clone the repo and run `npm install` (make sure you have [node.js](http://nodejs.org/) installed). You then can run karma locally to see how it works with `karma start`. Try saving a source file in the `src` folder or a test file in the `test` folder to see karma automatically re-run your tests. 
+
+By default, this example runs tests in Chrome and Firefox on your local machine, and you can add more browsers that you have installed in the `karma.conf.js` `browsers` array.
+
+### Karma and Sauce
+
+To use karma with Sauce, create a `sauce.json` file with your Sauce Labs username and access key (if you don't have an account, you can sign up [here](https://saucelabs.com/signup/plan/free)). You can reference the `sampleSauce.json` in the repo to see the format.
+
+### Karma and Sauce on CI
+
+It is cool to run Sauce locally while you develop, but even cooler to run with a Continuous Integration system on every commit to your codebase. To
---
+++ karma.conf-ci.js
@@ -0,0 +1,100 @@
+var fs = require('fs');
+
+module.exports = function(config) {
+
+  // Use ENV vars on Travis and sauce.json locally to get credentials
+  if (!process.env.SAUCE_USERNAME) {
+    if (!fs.existsSync('sauce.json')) {
+      console.log('Create a sauce.json with your credentials based on the sampleSauce.json file.');
+      process.exit(1);
+    } else {
+      process.env.SAUCE_USERNAME = require('./sauce').username;
+      process.env.SAUCE_ACCESS_KEY = require('./sauce').accessKey;
+    }
+  }
+
+  // Browsers to run on Sauce Labs
+  var customLaunchers = {
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome'
+    },
+    'SL_Firefox': {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '26'
+    },
+    'SL_Safari': {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.9',
+      version: '7'
+    },
+    'SL_IE_9': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 2008',
+      version: '9'
+    },
+    'SL_IE_10': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 2012',
+      version: '10'
+    },
+    'SL_IE_11': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    }
+  };
+
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'src/*.js',
+      'test/*.js'
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    sauceLabs: {
+      username: process.env.SAUCE_USERNAME,
+      accessKey: process.env.SAUCE_ACCESS_KEY,
+      testName: 'Karma and Sauce Labs demo',
+      startConnect: true,
+      build: process.env.TRAVIS_JOB_ID
+    },
+
+    customLaunchers: customLaunchers,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: Object.keys(customLaunchers)
+  });
+};
