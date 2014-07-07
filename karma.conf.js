234ab08688ae60178bc0bc316cf24e8c8ad51f44
Chris Wren <chris@saucelabs.com>
2014-07-06 21:05:16 -0700

    Fixed frameworks comment

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
 
 
     // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    // available frameworks: jasmine, qunit, mocha
     frameworks: ['jasmine'],
 
 
