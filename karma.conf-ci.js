fff1d6fcf25df4e6e7632e0f69be37ff3badc923
Chris Wren <chris@saucelabs.com>
2014-07-06 21:47:48 -0700

    Reduce number of browsers until concurrency config is added

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -14,33 +14,11 @@ module.exports = function(config) {
   }
 
   // Browsers to run on Sauce Labs
+  // Available platforms https://saucelabs.com/platforms
   var customLaunchers = {
     'SL_Chrome': {
       base: 'SauceLabs',
       browserName: 'chrome'
-    },
-    'SL_Firefox': {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-      version: '26'
-    },
-    'SL_Safari': {
-      base: 'SauceLabs',
-      browserName: 'safari',
-      platform: 'OS X 10.9',
-      version: '7'
-    },
-    'SL_IE_9': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 2008',
-      version: '9'
-    },
-    'SL_IE_10': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 2012',
-      version: '10'
     },
     'SL_IE_11': {
       base: 'SauceLabs',
