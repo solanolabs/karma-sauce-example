ea3c2f2f976a212be625c12204c444c61d0ba43e
Chris Wren <chriswrendev@gmail.com>
2014-09-01 09:11:37 -0700

    Only run tests in two browsers until concurrency is figured out

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -22,31 +22,7 @@ module.exports = function(config) {
     'SL_Firefox': {
       base: 'SauceLabs',
       browserName: 'firefox',
-      version: '26'
-    },
-    'SL_Safari': {
-      base: 'SauceLabs',
-      browserName: 'safari',
-      platform: 'OS X 10.9',
-      version: '7'
-    },
-    'SL_IE_9': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 2008',
-      version: '9'
-    },
-    'SL_IE_10': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 2012',
-      version: '10'
-    },
-    'SL_IE_11': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11'
+      version: '30'
     }
   };
 
